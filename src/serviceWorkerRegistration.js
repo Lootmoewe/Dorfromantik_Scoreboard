c77ee25a68076f544a79e1d5b6ae5f2f3782e261
lootmoewe <grewe.l@web.de>
2025-07-06 11:12:05 +0200

    Web App 6

--- src/serviceWorkerRegistration.js
+++ src/serviceWorkerRegistration.js
@@ -1,20 +1,14 @@
-// src/serviceWorkerRegistration.js
-
-// Simple template for CRA service worker registration
 export function register() {
-    if ('serviceWorker' in navigator) {
-      window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/service-worker.js').catch(registrationError => {
-          console.log('SW registration failed: ', registrationError);
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker
+        .register(`${process.env.PUBLIC_URL}/service-worker.js`)
+        .then(registration => {
+          console.log('Service Worker registered with scope:', registration.scope);
+        })
+        .catch(error => {
+          console.error('Service Worker registration failed:', error);
         });
-      });
-    }
+    });
   }
-  
-  export function unregister() {
-    if ('serviceWorker' in navigator) {
-      navigator.serviceWorker.ready.then(registration => {
-        registration.unregister();
-      });
-    }
-  }
+}
